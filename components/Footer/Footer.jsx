55fe782195c18d9aad4b50f4fc4b9f3e01b71f8a
Visiok09 <sm3729540@gmail.com>
2023-06-22 00:03:28 +0300

    update

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,15 +1,76 @@
-import React from 'react'
- 
-import Style from "./Footer.module.css"
+import React from "react";
+import Image from "next/image";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+  TiArrowSortedDown,
+  TiArrowSortedUp,
+} from "react-icons/ti";
+import { RiSendPlaneFill } from "react-icons/ri";
 
-
+import Style from "./Footer.module.css";
+import images from "../../img";
+import { Discover, HelpCenter } from "../NavBar/index";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={Style.footer}>
+      <div className={Style.footer_box}>
+        <div className={Style.footer_box_social}>
+          <Image src={images.logo} alt="footer_logo" width={100} height={100} />
+          <p>
+            The world’s first and largest digital marketplace for crypto
+            collectibles and non-fungible tokens (NFTs). Buy, sell, and discover
+            exclusive digital items.
+          </p>
 
-export default Footer
+          <div className={Style.footer_social}>
+            <a href="#">
+              <TiSocialFacebook />
+            </a>
+            <a href="#">
+              <TiSocialLinkedin />
+            </a>
+            <a href="#">
+              <TiSocialTwitter />
+            </a>
+            <a href="#">
+              <TiSocialYoutube />
+            </a>
+            <a href="#">
+              <TiSocialInstagram />
+            </a>
+          </div>
+        </div>
 
- 
+        <div className={Style.footer_box_discover}>
+          <h3>Discover</h3>
+          <Discover />
+        </div>
+
+        <div className={Style.footer_box_help}>
+          <h3>Helper Center</h3>
+          <HelpCenter />
+        </div>
+
+        <div className={Style.subcsribe}>
+          <h3>Subcribe</h3>
+
+          <div className={Style.subcsribe_box}>
+            <input type="email" placeholder="Enter your email *" />
+            <RiSendPlaneFill className={Style.subcsribe_box_send} />
+          </div>
+
+          <div className={Style.subcsribe_box_info}>
+            <p>Discover, collect, and sell...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
