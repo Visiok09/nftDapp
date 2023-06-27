94f48705649b616a4d06dd49052d1f13d0663ef8
Visiok09 <sm3729540@gmail.com>
2023-06-27 23:29:43 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,14 @@ import "../styles/globals.css";
 
 //internal import 
 
-import {NavBar} from "../components/componentsindex";
+import {NavBar,Footer } from "../components/componentsindex";
 
 const MyApp = ({Component, pageProps}) => (
     
     <div>
         <NavBar/>
         <Component {...pageProps}/>
+        <Footer/>
     </div>
     
 )
