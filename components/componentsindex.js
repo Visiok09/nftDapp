72d2fec94198ce409550dd91e70fdea33e5f80c8
Visiok09 <sm3729540@gmail.com>
2023-06-30 00:21:51 +0300

    update

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -5,5 +5,7 @@ import HeroSection from './HeroSection/HeroSection'
 import Service from './Service/Service'
 import BigNFTSlider from './BigNftSlider/BigNFTSlider'
 import Title from './Title/Title'
+import Category from './Category/Category'
+import Filter from './Filter/Filter'
 
-export {Button, Footer, NavBar, HeroSection, Service, BigNFTSlider, Title}
+export {Button, Footer, NavBar, HeroSection, Service, BigNFTSlider, Title, Category, Filter}
