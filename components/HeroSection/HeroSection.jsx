eb440cfdb47c9d1ed19b22db83980c6c41ea0d4a
Visiok09 <sm3729540@gmail.com>
2023-07-11 21:32:57 +0300

    update

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+
 import Image from "next/image";
+
+//Smart contract import
+import { NFTMarketplaceContext } from "../../Context/NFTMarketplaceContext";
 
 //INTERNAL IMPORT
 import Style from "./HeroSection.module.css";
@@ -7,10 +11,12 @@ import { Button } from "../componentsindex";
 import images from "../../img";
 
 const HeroSection = () => {
+  // const { titleData } = useContext(NFTMarketplaceContext);
   return (
     <div className={Style.heroSection}>
       <div className={Style.heroSection_box}>
         <div className={Style.heroSection_box_left}>
+          {/* <h1>{titleData} 🖼️</h1> */}
           <h1>Discover, collect, and sell NFTs 🖼️</h1>
           <p>
             Discover the most outstanding NTFs in all topics of life. Creative
